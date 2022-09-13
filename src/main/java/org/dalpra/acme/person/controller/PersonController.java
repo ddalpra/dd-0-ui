a6f018e5aaa6f5f8485efba3a7a5f4785605752b
ddalpra <d.dalpra86@gmail.com>
2022-09-13 22:51:33 +0200

    aggiunta la pagina dashboard e della pagina person

---
+++ src/main/java/org/dalpra/acme/person/controller/PersonController.java
@@ -0,0 +1,81 @@
+package org.dalpra.acme.person.controller;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.dalpra.acme.person.entity.Person;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named("personController")
+@SessionScoped
+public class PersonController implements Serializable {
+    private List<Person> personList;
+    private Person person;
+    private transient Client client;
+    static String BASE_URL = "http://localhost:9000/";
+
+    public PersonController() {
+        FacesContext fc = FacesContext.getCurrentInstance();
+
+    }
+
+    public List<Person> getPersonList() {
+        ResteasyClient client = (ResteasyClient)ClientBuilder.newClient();
+        ResteasyWebTarget target = client.target(BASE_URL+"person/");
+        Invocation.Builder request = target.request();
+        Response response = null;
+        try
+        {
+            response = request.get();
+            personList = response.readEntity(new GenericType<List<Person>>() {});
+
+        }
+        finally
+        {
+            response.close();
+            client.close();
+        }
+
+        return personList;
+    }
+
+    public void setPersonList(List<Person> personList) {
+        this.personList = personList;
+    }
+
+    public Person getPerson() {
+        ResteasyClient client = (ResteasyClient)ClientBuilder.newClient();
+        ResteasyWebTarget target = client.target(BASE_URL+"person/");
+        Invocation.Builder request = target.request();
+        Response response = null;
+        try
+        {
+            response = request.get();
+            personList = response.readEntity(new GenericType<List<Person>>() {});
+
+        }
+        finally
+        {
+            response.close();
+            client.close();
+        }
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+
+
+}
---
+++ src/main/java/org/dalpra/acme/person/entity/Person.java
@@ -0,0 +1,50 @@
+package org.dalpra.acme.person.entity;
+
+public class Person {
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+
+    public Person(){
+    }
+
+    public Person(String firstname, String lastname, String username, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.email = email;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
